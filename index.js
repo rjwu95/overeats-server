8cfa10a79236453de00a66cba378ed884c094775
DoheeKang <doheee.kang@gmail.com>
2019-02-08 21:42:45 +0900

    temp commit

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const userRouter = require('./routes/user');
-
+const menuRouter = require('./routes/menu');
 const app = express();
 
 app.use(express.static('dist'));
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.end('Hello over eats!');
+});
 app.use('/users', userRouter);
-
+app.use('/menus', menuRouter);
 app.listen(8080, () => console.log('Listening on port 8080!'));
 
 module.exports = app;

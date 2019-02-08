8b20ec77cf72cc50037edfc3ea500987292ded51
DoheeKang <doheee.kang@gmail.com>
2019-02-08 17:31:54 +0900

    Add property 'ordered' to user schema

--- models/users.js
+++ models/users.js
@@ -2,11 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    name: String,
-    email: String,
-    password: String,
-    phoneNumber: String,
-    ordered: Array
+  name: String,
+  email: String,
+  password: String,
+  phoneNumber: String,
+  ordered: Array,
+  restaurantKey: { type: String, default: null }
 });
 
 module.exports = mongoose.model('user', userSchema);

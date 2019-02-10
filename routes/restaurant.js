8c9e8248609cf275cd3c2fc5f1c054c5cc7d858a
kimbeomgyu <qjarb1994@gmail.com>
2019-02-11 00:35:21 +0900

    restaurant.js API add category get

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,10 +1,12 @@
 const Restaurant = require('../models/restaurant');
-
+const list = require('./category');
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
+
 router.get('/:category', (req, res) => {
   let { category } = req.params;
+  category = list[category];
   mongoose.connect('mongodb://13.125.252.142:38380/overEats', {
     useNewUrlParser: true
   });
@@ -14,38 +16,14 @@ router.get('/:category', (req, res) => {
   db.once('open', function() {
     console.log('we are connected!');
     //check there's same category
-    Restaurant.find({ category: req.body.category }, async (err, user) => {
+
+    Restaurant.find({ category: category }, async (err, rest) => {
+      // find category.js
       if (err) {
         return res.status(500).json({ error: err });
       }
-      switch (category) {
-        case '한식':
-          break;
-        case '분식':
-          break;
-        case '돈까스,회,일식':
-          break;
-        case '치킨':
-          break;
-        case '피자':
-          break;
-        case '도시락':
-          break;
-        case '족발,보쌈':
-          break;
-        case '야식':
-          break;
-        case '중국집':
-          break;
-        case '찜,탕':
-          break;
-        case '카페,디저트':
-          break;
-        case '패스트푸드':
-          break;
-        case '세계음식':
-          break;
-      }
+      res.writeHead(200);
+      res.end(JSON.stringify(rest));
     });
   });
 });

594db17120d2b6f88128e896874c6cf7e558edf4
kimbeomgyu <qjarb1994@gmail.com>
2019-02-11 00:30:42 +0900

    create category object module.exports

---
+++ routes/category.js
@@ -0,0 +1,17 @@
+const category = {
+  korean: '한식',
+  snack: '분식',
+  japanese: '돈까스,회,일식',
+  chicken: '치킨',
+  pizza: '피자',
+  lunchbox: '도시락',
+  pork: '족발,보쌈',
+  night: '야식',
+  chinese: '중국집',
+  soup: '찜,탕',
+  dessert: '카페,디저트',
+  junk: '패스트푸드',
+  world: '세계음식'
+};
+
+module.exports = category;

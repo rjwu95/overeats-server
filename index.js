6152809389049b4e374f1739a3d3a7a9915044cd
kimbeomgyu <qjarb1994@gmail.com>
2019-02-12 16:23:13 +0900

    cors 수정

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const userRouter = require('./routes/user');
 const restRouter = require('./routes/restaurant');
 const app = express();
 
 app.use(express.static('dist'));
 app.use(bodyParser.json());
-
+app.all('/*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
 app.get('/', (req, res) => {
   res.statusCode = 200;
   res.end('Hello over eats!');

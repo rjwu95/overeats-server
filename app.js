c4eee46b13649614f505765dab9969cf9e79c990
DoheeKang <doheee.kang@gmail.com>
2019-02-13 15:28:51 +0900

    Change index.js to app.js Now first page is app.js

---
+++ app.js
@@ -0,0 +1,53 @@
+/* =======================
+    LOAD THE DEPENDENCIES
+==========================*/
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+/* =======================
+    LOAD THE CONFIG
+==========================*/
+const config = require('./config');
+const port = process.env.PORT || 3000;
+
+/* =======================
+    LOAD THE ROUTER
+==========================*/
+const userRouter = require('./routes/user/');
+const restRouter = require('./routes/restaurant');
+
+/* =======================
+    EXPRESS CONFIGURATION
+==========================*/
+const app = express();
+
+// parse JSON and url-encoded query
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// set the secret key variable for jwt
+app.set('jwt-secret', config.secret);
+// handle cors error
+app.use(cors());
+
+// index page, just for testing
+app.get('/', (req, res) => {
+  res.end('Hello over eats!');
+});
+
+// configure api router
+app.use('/users', userRouter);
+app.use('/restaurants', restRouter);
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
+
+/* =======================
+    CONNECT TO MONGODB SERVER
+==========================*/
+mongoose.connect(config.mongodbUri);
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => {
+  console.log('connected to mongodb server');
+});

833d8c32d94cddaa2f29504dbf016fa33d3eb198
kimbeomgyu <qjarb1994@gmail.com>
2019-02-15 10:49:21 +0900

    Add date,order_id * restaurant/controller.js/payment

--- routes/restaurant/controller.js
+++ routes/restaurant/controller.js
@@ -1,6 +1,5 @@
 const categoryList = require('../category');
 const Restaurant = require('../../models/restaurant');
-const jwt = require('jsonwebtoken');
 
 /* category & information */
 exports.category = (req, res) => {
@@ -21,19 +20,48 @@ exports.category = (req, res) => {
 
 /* Payment */
 exports.payment = (req, res) => {
-  const token = req.headers['x-access-token'] || req.query.token;
-  const order_id = 200712344578;
-  const date = new Date(new Date().getTime()).toString();
+  const token = req.headers['x-access-token'];
   //식당ID, 주문내역 ={ 주문메뉴,주문개수,주문가격}
   let { _id, ordered } = req.body;
-  io.emit(_id, {
-    phoneNumber,
-    ordered,
-    name,
-    order_id,
-    date
+  let io = req.app.get('socketio');
+
+  jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
+    if (err) {
+      // if it has failed to verify, it will return an error message
+      return res.status(401).json({
+        success: false,
+        message: err.message
+      });
+    } else {
+      const { email, phoneNumber } = decoded;
+      let date = new Date();
+      date = `${date.getFullYear()}년 ${date.getMonth() +
+        1}월 ${date.getDate()}일 ${date.getHours()}시 ${date.getMinutes()}분`;
+      const order_id =
+        Number(String(Date.now()).slice(5)) + phoneNumber.slice(4, 8);
+      // if token is valid, it will respond with its info
+      User.find({ email }, async (err, user) => {
+        // if email is exist
+        if (user.length > 0) {
+          //update user
+          let { phoneNumber, name } = user[0];
+          io.emit(_id, {
+            phoneNumber,
+            ordered,
+            name,
+            order_id,
+            date
+          });
+          res.end(JSON.stringify({ order_id, ordered, date }));
+        } else {
+          //if email is non-exist
+          res.writeHead(401);
+          res.end('Please login info');
+          return;
+        }
+      });
+    }
   });
-  res.end(JSON.stringify({ order_id, ordered, date }));
 };
 
 /* Finish delivery */

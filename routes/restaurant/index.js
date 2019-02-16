d2542a3985dc0ff5b14d7c844bd33305247df372
DoheeKang <doheee.kang@gmail.com>
2019-02-16 19:34:52 +0900

    change middleware name

--- routes/restaurant/index.js
+++ routes/restaurant/index.js
@@ -4,6 +4,6 @@ const authMiddleware = require('../../middleware/auth');
 
 router.get('/delivery/:restaurantKey/:order_id', controller.delivery);
 router.get('/:category/:address', controller.category);
-router.post('/payment', authMiddleware.checkToken, controller.payment);
+router.post('/payment', authMiddleware.checkExpiredToken, controller.payment);
 router.post('/review', controller.review);
 module.exports = router;
--- routes/user/index.js
+++ routes/user/index.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../../middleware/auth');
 router.post('/signup', controller.signup);
 router.post('/signin', controller.signin);
 
-router.get('/info', authMiddleware.checkToken, controller.info);
-router.get('/signout', authMiddleware.checkToken, controller.signout);
+router.get('/info', authMiddleware.checkExpiredToken, controller.info);
+router.get('/signout', authMiddleware.checkExpiredToken, controller.signout);
 
 module.exports = router;

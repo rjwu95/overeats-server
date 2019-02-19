ff7b630157acd31343bee2d14cba46bf49c450fb
DoheeKang <doheee.kang@gmail.com>
2019-02-19 17:11:20 +0900

    Modify jest test * use done in callback for finish async test

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const cors = require('cors');
 /* =======================
     LOAD THE CONFIG
 ==========================*/
+//must be in git ignore!!
 const config = require('./config');
 
 /* =======================
--- test/test.js
+++ test/test.js
@@ -1,59 +1,60 @@
 const request = require('request');
 
-test('check the sever is on', () => {
+test('check the sever is on', done => {
   request(
     'http://ec2-34-201-173-255.compute-1.amazonaws.com:8080/',
     (err, res, body) => {
       expect(body).toBe('Hello over eats!');
+      done();
     }
   );
 });
 
-test('check signup and signup', () => {
+test('check signup', done => {
   request.post(
     'http://ec2-34-201-173-255.compute-1.amazonaws.com:8080/users/signup',
     {
       json: {
-        email: 'vscode@gmail.com',
+        email: 'qwerty@gmail.com',
         password: '1234',
         name: 'test',
-        phoneNumber: '010-7090-9090'
+        phoneNumber: '010-7090-1212'
       }
     },
-    function(error, response, body) {
+    function(error, res, body) {
+      expect(res.statusCode).toBe(200);
       expect(body).toBe('nice to meet you');
+      done();
     }
   );
 });
 
-test('check sing in', () => {
+test('check sign in', done => {
   request.post(
     'http://ec2-34-201-173-255.compute-1.amazonaws.com:8080/users/signin',
     {
       json: {
-        email: 'vscode@gmail.com',
-        password: '1234',
-        name: 'test',
-        phoneNumber: '010-7090-9090'
+        email: 'qwerty@gmail.com',
+        password: '1234'
       }
     },
     (err, res, body) => {
+      expect(res.statusCode).toBe(200);
       expect(Object.keys(res.headers)).toContain('refresh-token');
       expect(Object.keys(res.headers)).toContain('access-token');
       expect(body).toBe('okay');
+      done();
     }
   );
 });
 
-test('check signout', () => {
+test('check signout', done => {
   request.post(
     'http://ec2-34-201-173-255.compute-1.amazonaws.com:8080/users/signin',
     {
       json: {
-        email: 'vscode@gmail.com',
-        password: '1234',
-        name: 'test',
-        phoneNumber: '010-7090-9090'
+        email: 'qwerty@gmail.com',
+        password: '1234'
       }
     },
     (err, res, body) => {
@@ -61,11 +62,13 @@ test('check signout', () => {
         `http://ec2-34-201-173-255.compute-1.amazonaws.com:8080/users/signout/?token=${
           res.headers['access-token']
         }`,
-        function(error, response, body) {
+        (error, response, body) => {
           expect(response.statusCode).toBe(200);
           expect(body).toBe('ok');
+          done();
         }
       );
+      done();
     }
   );
 });
